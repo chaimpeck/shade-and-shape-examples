e8248f224484dae8fad5e958fae26f7645b9526f
Chaim Peck <chaimpeck@gmail.com>
2024-01-04 04:44:34 -0500

    update for demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import glob from "glob";
+import { basename, resolve } from "path";
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
   base: "/shade-and-shape-examples/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        ...glob.sync(resolve(__dirname, "examples/*.html")).reduce(
+          (inputs: Record<string, string>, p: string) => ({
+            ...inputs,
+            [basename(p).split(".")[0]]: p,
+          }),
+          {}
+        ),
+      },
+    },
+  },
   plugins: [solid()],
 });
